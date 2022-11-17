46652dff4f0b741b56642e7ecec5d40ac3d1e77a
Sujitdhanaji123 <dhanajisubhadranandan123@gmail.com>
2022-11-17 11:10:50 +0530

    FIR -1 : - Create required package structure

---
+++ FirstGitJiraApp/src/test/java/com/BikkadIT/FirstGitJiraApp/FirstGitJiraAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BikkadIT.FirstGitJiraApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstGitJiraAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
